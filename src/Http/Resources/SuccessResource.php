7c8a358f81e5b9ec9ceb5667c73f3f1288213b14
Matias Camiletti <matias.camiletti@gmail.com>
2023-12-18 14:13:16 -0300

    Add success resource.

---
+++ src/Http/Resources/SuccessResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tots\Core\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SuccessResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'success' => true,
+            'message' => $this->resource,
+        ];
+    }
+}
